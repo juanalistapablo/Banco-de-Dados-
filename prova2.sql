af940834b5a5cbe818093ed613acdaa44ed1b5e0
JUANPABLOMONDEGO <juanmondego208@gmail.com>
2026-01-31 11:40:41 -0300

    Create prova2.sql

---
+++ prova2.sql
@@ -0,0 +1,16 @@
+/* 1 Snapchat*/
+SELECT
+    a.age_bucket,
+    (t1.time_open / (t1.time_send + t1.time_open)) * 100.0 AS open_perc,
+    (t1.time_send / (t1.time_send + t1.time_open)) * 100.0 AS send_perc
+FROM (
+    SELECT 
+        user_id,
+        MAX(CASE WHEN activity_type = 'open' THEN time_spent END) AS time_open,
+        MAX(CASE WHEN activity_type = 'send' THEN time_spent END) AS time_send
+    FROM activities
+    WHERE activity_type IN ('open', 'send')
+    GROUP BY user_id
+) t1
+JOIN age_breakdown a 
+ON t1.user_id = a.user_id;
