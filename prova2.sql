175a30c97749cea0abc859169b818ce83aa1cdc7
JUANPABLOMONDEGO <juanmondego208@gmail.com>
2026-02-02 12:00:33 -0300

    Update prova2.sql

--- prova2.sql
+++ prova2.sql
@@ -26,3 +26,11 @@ count(case_id)
 FROM callers
 GROUP BY policy_holder_id
 HAVING count(case_id) >= 3)t1
+/* 3 Prova da Entrevista de SQL da Meta*/
+SELECT 
+    app_id,
+    100.0 * COUNT(IF(event_type = 'click', 1, NULL)) / 
+    IF(COUNT(IF(event_type = 'impression', 1, NULL)) = 0, NULL, COUNT(IF(event_type = 'impression', 1, NULL))) AS ctr
+FROM events
+WHERE timestamp >= '2022-01-01' AND timestamp <= '2022-12-31'
+GROUP BY app_id;
